c77f9ffa111a65397a0dfb3e27e2cce3e2822de8
stupk <bozzya7@gmail.com>
2023-10-19 10:56:19 +0200

    added healthcheck endpoint

--- project/views.py
+++ project/views.py
@@ -3,5 +3,12 @@ from flask import jsonify
 import datetime
 
 
+@app.route("/healthcheck")
+def healthcheck():
+    response = {
+        "current date": datetime.datetime.now().strftime("%d:%m:%Y"),
+        "status": "OK"
+    }
 
+    return jsonify(response), 200
 
