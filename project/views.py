24fdc6fc8921911a77a39c962b65988bac1a20a2
stupk <bozzya7@gmail.com>
2023-11-17 12:27:38 +0100

    Added user and category endpoints

--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 import random
 import uuid
 
+
 from project import app
 from flask import jsonify, request, g
 import datetime
@@ -24,63 +25,73 @@ def healthcheck():
 def get_user():
     user_id = request.args.get("user_id", None)
     if user_id:
-        return users[user_id]
-    return jsonify({"response": f"user {user_id} does not exist"})
+        user = users.get(user_id, None)
+        if user:
+            return users[user_id], 200
+        return jsonify({"response": f"user {user_id} does not exist"}), 404
+    return jsonify({"response": "Required argument \'used_id\' wasn't passed"}), 400
 
 
 @app.post("/user")
 def create_user():
-    username = request.get_json()['username']
-    user_id = uuid.uuid4().hex
-    user = {"id": user_id, "username": username}
-    users[user_id] = user
-    return user
+    username = request.get_json().get("username", None)
+    if username:
+        user_id = uuid.uuid4().hex
+        user = {"id": user_id, "username": username}
+        users[user_id] = user
+        return user, 201
+    return jsonify({"response": "Required data \'username\' wasn't passed"}), 400
 
 
 @app.delete("/user")
 def delete_user():
     user_id = request.args.get("user_id", None)
     if user_id:
-        user = users.pop(user_id)
-        response = {
-            "response": f'{user["username"]} was deleted'
-        }
-        return jsonify(response)
-    return jsonify({"response": f"user {user_id} does not exist"})
+        try:
+            user = users.pop(user_id)
+            return jsonify({"response": f'{user["username"]} was deleted'}), 200
+        except KeyError as e:
+            return jsonify({"response": f"user {user_id} does not exist"}), 404
+    return jsonify({"response": "required argument \'used_id\' wasn't passed"}), 400
 
 
 @app.get("/users")
 def get_users():
-    return jsonify(users)
+    return jsonify({users.values()}), 200
 
 
 @app.get("/category")
 def get_category():
     category_id = request.args.get('category_id', None)
     if category_id:
-        return categories[category_id]
-    return jsonify({"response": f"category {category_id} does not exist"})
+        category = categories.get(category_id, None)
+        if category:
+            return categories[category_id], 200
+        return jsonify({"response": f"category {category_id} does not exist"}), 404
+    return jsonify({"response": "Required argument \'category_id\' wasn't passed"}), 400
 
 
 @app.post("/category")
-def post_category():
-    category_name = request.get_json()['category']
-    cat_id = uuid.uuid4().hex
-    category = {"id": cat_id, "category": category_name}
-    categories[cat_id] = category
-    return jsonify({cat_id: category})
+def create_category():
+    category_name = request.get_json().get("category", None)
+    if category_name:
+        cat_id = uuid.uuid4().hex
+        category = {"id": cat_id, "category": category_name}
+        categories[cat_id] = category
+        return category, 201
+    return jsonify({"response": "Required data \'category\' wasn't passed"}), 400
 
 
 @app.delete("/category")
 def delete_category():
     category_id = request.args.get("category_id", None)
     if category_id:
-        category = categories.pop(category_id)
-        response = {
-            "response": f'{category["category"]} was deleted'
-        }
-        return jsonify(response)
-    return jsonify({"response": f"category {category_id} does not exist"})
+        try:
+            category = categories.pop(category_id)
+            return jsonify({"response": f'{category["category"]} was deleted'}), 200
+        except KeyError as e:
+            return jsonify({"response": f"category {category_id} does not exist"}), 404
+    return jsonify({"response": "required argument \'category_id\' wasn't passed"}), 400
 
 
 @app.post("/record")
@@ -95,3 +106,10 @@ def create_record():
     records[record_id] = record
     return record
 
+
+# @app.delete
+# def delete_record():
+#     pass
+#     # record_id = request.args.get("request", None)
+#     # if record_id
+#
