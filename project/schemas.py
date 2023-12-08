58e078613ebdd768008300b59e91ead4f3543be9
stupk <bozzya7@gmail.com>
2023-12-08 10:01:19 +0100

    feat: [add schemas and sqlalchemy models]

---
+++ project/schemas.py
@@ -0,0 +1,19 @@
+from marshmallow import schema, fields
+
+
+class UserSchema(schema):
+    id = fields.Str(dump_only=True)
+    username = fields.Str(required=True)
+
+
+class CategorySchema(schema):
+    id = fields.Str(dump_only=True)
+    category = fields.Str(required=True)
+
+
+class RecordSchema(schema):
+    id = fields.Str(dump_only=True)
+    user = fields.Str(required=True)
+    category = fields.Str(required=True)
+    
+
